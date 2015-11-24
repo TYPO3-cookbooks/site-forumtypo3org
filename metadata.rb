8a5dc89914a0e1733834bb65d9a80991f71a3bea
Steffen Gebert <steffen.gebert@typo3.org>
2015-11-24 09:44:41 +0100

    Bump version to 0.0.2

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer       "TYPO3 Association"
 maintainer_email "steffen.gebert@typo3.org"
 license          "Apache 2.0"
 description      "Installs/Configures forum.typo3.org"
-version "0.0.1"
+version          "0.0.2"
 
 depends          "database", "= 1.3.12"
 
